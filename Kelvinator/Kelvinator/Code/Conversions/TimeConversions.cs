502a0a57bbe4b5732541264ead709f11f4508911
Luc Schulz <luc@lucschulz.com>
2019-09-28 19:02:00 -0400

    Update TimeConversions.cs

--- Kelvinator/Kelvinator/Code/Conversions/TimeConversions.cs
+++ Kelvinator/Kelvinator/Code/Conversions/TimeConversions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using static KelvinatorX.Code.Enums;
 
 namespace KelvinatorX.Code.Conversions
@@ -13,17 +14,22 @@ namespace KelvinatorX.Code.Conversions
             ConversionFactors = GetConversionFactors();
         }
 
+
+        /// <summary>
+        /// Base unit is the minute.
+        /// </summary>
+        /// <returns>Returns the conversion factor for the output value.</returns>
         public override Dictionary<object, double> GetConversionFactors()
         {
             var t = new Dictionary<object, double>
             {
-                { TimeUnits.Hour, 1.0 },
                 { TimeUnits.Picosecond, 3.6e+15 },
-                { TimeUnits.Nanosecond, 3600002880000.6713867 },
-                { TimeUnits.Microsecond, 3.6e+9 },
-                { TimeUnits.Millisecond, 3.6e+6 },
-                { TimeUnits.Second, 3600 },
-                { TimeUnits.Minute, 60 },
+                { TimeUnits.Nanosecond, 6e+10 },
+                { TimeUnits.Microsecond, 6e+7 },
+                { TimeUnits.Millisecond, 6e+4 },
+                { TimeUnits.Second, 60 },
+                { TimeUnits.Minute, 1.0 },
+                { TimeUnits.Hour, 0.016667 },
                 { TimeUnits.Day, 0.0416667 },
                 { TimeUnits.Week, 0.00595238 },
                 { TimeUnits.Month, 0.00136986260837 },
@@ -40,7 +46,7 @@ namespace KelvinatorX.Code.Conversions
             ConversionFactors.TryGetValue((TimeUnits)base.FromUnit, out double fromFactor);
             ConversionFactors.TryGetValue((TimeUnits)base.ToUnit, out double toFactor);
 
-            return input * fromFactor / toFactor;
+            return Math.Round(input * fromFactor / toFactor, 1, MidpointRounding.AwayFromZero);
         }
     }
 }
